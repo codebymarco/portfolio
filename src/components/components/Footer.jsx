1aa1187f14aaabd037e37512fced0f0690e2082b
RamcharanMarco <miguelmarcoramcharan@gmail.com>
2024-06-10 20:25:47 +0200

    added

--- src/components/components/Footer.jsx
+++ src/components/components/Footer.jsx
@@ -1,11 +1,24 @@
 import "../../styles/footer.css";
 
 const Footer = () => {
+  const handleClick2 = () => {
+    const email = "miguelmarcoramcharan@gmail.com";
+    const subject = "Subject Here"; // You can customize the subject if needed
+    const body = "Body of the email goes here"; // You can customize the body if needed
+    window.open(
+      `mailto:${email}?subject=${encodeURIComponent(
+        subject
+      )}&body=${encodeURIComponent(body)}`
+    );
+  };
+
   return (
     <footer>
-      <span>github</span>
-      <span>miguelmarcormacharan@gmail.com</span>
-      <span>linkdn</span>
+      <a href="https://github.com/codebymarco" target="_blank">github</a>
+      <span onClick={handleClick2}>miguelmarcormacharan@gmail.com</span>
+      <a href="https://www.linkedin.com/in/miguelmarco-ramcharan-34b04a277" target="_blank">
+        linkden
+      </a>
     </footer>
   );
 };
