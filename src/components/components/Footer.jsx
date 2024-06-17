639903219da2c8d5a312bafbfeb6cca54db7feb2
RamcharanMarco <miguelmarcoramcharan@gmail.com>
2024-06-17 13:36:53 +0200

    added download resume and we

--- src/components/components/Footer.jsx
+++ src/components/components/Footer.jsx
@@ -1,23 +1,22 @@
+import React from 'react';
 import "../../styles/footer.css";
+import resumePDF from "../../../public/resume.pdf"// Adjust the path based on your actual file location
 
 const Footer = () => {
-  const handleClick2 = () => {
-    const email = "miguelmarcoramcharan@gmail.com";
-    const subject = "Subject Here"; // You can customize the subject if needed
-    const body = "Body of the email goes here"; // You can customize the body if needed
-    window.open(
-      `mailto:${email}?subject=${encodeURIComponent(
-        subject
-      )}&body=${encodeURIComponent(body)}`
-    );
+  const handleDownloadResume = () => {
+    const downloadLink = document.createElement('a');
+    downloadLink.href = resumePDF;
+    downloadLink.download = 'resume.pdf';
+    downloadLink.click();
   };
 
   return (
     <footer>
-      <a href="https://github.com/codebymarco" target="_blank">github</a>
-      <span onClick={handleClick2}>miguelmarcormacharan@gmail.com</span>
-      <a href="https://www.linkedin.com/in/miguelmarco-ramcharan-34b04a277" target="_blank">
-        linkden
+      <a href="https://github.com/codebymarco" target="_blank" rel="noopener noreferrer">github</a>
+      <span onClick={handleDownloadResume}>resume</span>
+      <a href="mailto:miguelmarcoramcharan@gmail.com">miguelmarcoramcharan@gmail.com</a>
+      <a href="https://www.linkedin.com/in/miguelmarco-ramcharan-34b04a277" target="_blank" rel="noopener noreferrer">
+        LinkedIn
       </a>
     </footer>
   );
