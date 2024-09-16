960d4839d35f4877392244f6e1216957e20ee7d3
RamcharanMarco <miguelmarcoramcharan@gmail.com>
2024-09-16 22:24:26 +0200

    added

--- src/components/pages/apps/LinkBio.jsx
+++ src/components/pages/apps/LinkBio.jsx
@@ -1,6 +1,17 @@
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import "../../../styles/app.css";
+import { FaCss3Alt, FaHtml5, FaNodeJs } from "react-icons/fa";
+import {
+  SiMongodb,
+  SiOpenstreetmap,
+  SiRabbitmq,
+  SiRender,
+  SiTypescript,
+} from "react-icons/si";
+import { IoLogoVercel } from "react-icons/io5";
+import { FiGithub } from "react-icons/fi";
+import img from "../../../assets/linkbio.png";
 
 const LinkBio = () => {
   return (
@@ -18,8 +29,13 @@ const LinkBio = () => {
         </motion.h1>
       </div>
       <div className="app-links">
-        <Link to="/">view site</Link>
-        <Link to="/">repo</Link>
+        <a href="https://indeed-console.vercel.app/" target="_blank">
+          view site
+        </a>
+        <a href="https://github.com/codebymarco/indeed_backend" target="_blank">
+          <FiGithub color="white" />
+          repo
+        </a>
       </div>
       <div className="app-container">
         <motion.div
@@ -27,24 +43,22 @@ const LinkBio = () => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 0.5, duration: 0.5 }}
-        ></motion.div>
+        >
+          <img src={img} alt="akfjhfl" />
+        </motion.div>
         <div className="app-detials">
           <div className="app-details-box description">
             <h2>description</h2>
             <p>
-              LinkBio is an innovative app designed to streamline your online
-              presence by consolidating all your links onto a single,
-              customizable page. Whether you're an influencer, a business, or an
-              individual looking to share multiple links efficiently, LinkBio
-              offers a user-friendly platform to manage and showcase your
-              digital footprint.
-            </p>
-            <p>
-              With LinkBio, you can create a personalized page that includes
-              links to your social media profiles, websites, blogs, online
-              stores, and any other online resources you want to share. The app
-              provides various templates and customization options, allowing you
-              to design a page that reflects your unique style and branding.
+              <span style={{ color: "dodgerblue" }}>LINKBIO</span> is a
+              versatile application designed to help users create and manage a
+              personalized bio link in one place. Whether you're a content
+              creator wanting to share multiple links or a business looking to
+              streamline your online presence, LinkBio provides an efficient
+              platform to consolidate and showcase your most important links.
+              With its user-friendly interface and customizable features,
+              LinkBio enhances how you connect with your audience and present
+              your online identity.
             </p>
           </div>
           <div className="divider"></div>
@@ -52,30 +66,69 @@ const LinkBio = () => {
           <div className="app-details-box">
             <h2>technologies used</h2>
             <div className="app-detials-box-technologies">
-              <div className="box">html</div>
-              <div className="box">css</div>
-              <div className="box">typescript</div>
-              <div className="box">node</div>
-              <div className="box">nosql</div>
-              <div className="box">mongodb</div>
-              <div className="box">rabbitmq</div>
+              <div className="box">
+                <FaHtml5 color="E34F26" />
+                html
+              </div>
+              <div className="box">
+                {" "}
+                <FaCss3Alt color="1572B6" />
+                css
+              </div>
+              <div className="box">
+                {" "}
+                <SiTypescript color="007ACC" /> typescript
+              </div>
+              <div className="box">
+                <FaNodeJs color="#339933" />
+                node
+              </div>
+              <div className="box">
+                <SiMongodb color="#47A248" />
+                nosql
+              </div>
+              <div className="box">
+                <SiMongodb color="#47A248" />
+                mongodb
+              </div>
+              <div className="box">
+                <SiRabbitmq color="#FF6600" />
+                rabbitmq
+              </div>
+              <div className="box">
+                <SiOpenstreetmap color="#47A248" />
+                open street map
+              </div>
             </div>
           </div>
           <div className="divider"></div>
           <div className="app-details-box">
             <h2>hosting</h2>
             <div className="app-detials-box-technologies">
-              <div className="box">vercel</div>
-              <div className="box">render</div>
-              <div className="box">mongoatls</div>
+              <div className="box">
+                {" "}
+                <IoLogoVercel color="white" />
+                vercel
+              </div>
+              <div className="box">
+                <SiRender color="yellow" />
+                render
+              </div>
+              <div className="box">
+                <SiMongodb color="#47A248" />
+                mongo atlas
+              </div>
             </div>
           </div>
           <div className="divider"></div>
           <div className="app-details-box">
             <h2>links</h2>
             <div className="app-detials-box-technologies">
-              <div className="box">live app</div>
-              <div className="box">git repo</div>
+              <div className="box hover">live app</div>
+              <div className="box hover">
+                <FiGithub color="white" />
+                git repo
+              </div>
             </div>
           </div>
         </div>
