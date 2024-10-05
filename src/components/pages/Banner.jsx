52ebff4404f9ed2794d37059106dbce7996c7b44
RamcharanMarco <miguelmarcoramcharan@gmail.com>
2024-10-05 15:26:06 +0200

    added

--- src/components/pages/Banner.jsx
+++ src/components/pages/Banner.jsx
@@ -2,8 +2,16 @@ import React from "react";
 import { motion } from "framer-motion";
 import "../../styles/banner.css";
 import image from "../../assets/image.png";
+import { useNavigate } from "react-router-dom";
 
 const Banner = () => {
+
+  const navigate = useNavigate()
+
+  const showAbout = () =>{
+    navigate('/about')
+  }
+
   return (
     <div className="banner">
       <motion.img
@@ -13,7 +21,7 @@ const Banner = () => {
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 5 }}
       />
-      <button id="banner-about">about</button>
+      <button onClick={() => showAbout()} id="banner-about">about</button>
     </div>
   );
 };
