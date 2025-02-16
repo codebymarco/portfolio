26f88a5cfdd174adc91368c846f0fa367724c371
RamcharanMarco <miguelmarcoramcharan@gmail.com>
2025-02-16 19:50:13 +0200

    made more changes

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,17 +1,53 @@
-import React from 'react'
-
 const About = () => {
   return (
     <div>
-      <h1>tinder about here</h1>
-      <p>follow the layout</p>
-      <p>i will need other stuff as well
-        <span>should we allow them so swipe right or left</span>
-        <span>if they swipe left, say ur ugly</span>
-        <span>if they swipe right, say ur really intelligent</span>
-      </p>
+      {/* top */}
+      <div>
+        <span>tinder logo</span>
+        <div>
+          <span>notifications</span>
+          <span>filter</span>
+          <span>super like</span>
+        </div>
+      </div>
+      {/* photo div */}
+      <div>
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div>photo here</div>
+        </div>
+      </div>
+      {/* looking for div */}
+      <div>
+        <div>
+          <span>sreach icon</span>
+          <span>lokking for</span>
+        </div>
+        <span>what i am looking for</span>
+      </div>
+      {/* about me div */}
+      <div>
+        <div>
+          <span>about me</span>
+        </div>
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea,
+          recusandae quisquam voluptas iusto repellat accusamus iure vero odit
+          eligendi nulla.
+        </span>
+      </div>
+      {/* essentials div */}
+      <div>
+        <div>
+          <span>essentials</span>
+        </div>
+        <span>all my stats here</span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
