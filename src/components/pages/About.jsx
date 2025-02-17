3aabc798ff5e0ce7b0ad621b306b1c91b0e50f6c
RamcharanMarco <miguelmarcoramcharan@gmail.com>
2025-02-17 22:03:19 +0200

    added

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -23,7 +23,10 @@ const About = () => {
           <div></div>
         </div>
         <div className="photo-section">
-          <img src="https://th.bing.com/th/id/R.6e5c581e51eb9ee2d5b4f079066ba884?rik=bdcSOJNU1VnqfA&riu=http%3a%2f%2fcdn.designbump.com%2fwp-content%2fuploads%2f2015%2f05%2fenhanced-buzz-wide-29647-1394478394-12.jpg&ehk=JPk1fLlgcqoRCZJeVV9N46o5que23SXtaTB3BIzCQWM%3d&risl=&pid=ImgRaw&r=0" alt="" />
+          <img
+            src="https://th.bing.com/th/id/R.6e5c581e51eb9ee2d5b4f079066ba884?rik=bdcSOJNU1VnqfA&riu=http%3a%2f%2fcdn.designbump.com%2fwp-content%2fuploads%2f2015%2f05%2fenhanced-buzz-wide-29647-1394478394-12.jpg&ehk=JPk1fLlgcqoRCZJeVV9N46o5que23SXtaTB3BIzCQWM%3d&risl=&pid=ImgRaw&r=0"
+            alt=""
+          />
         </div>
       </div>
       {/* looking for div */}
@@ -33,8 +36,8 @@ const About = () => {
           <span>lokking for</span>
         </div>
         <span>
-          I'm looking to work with people that will help me grow as a persona and
-          a programmer, as well as learn stuff about new cultures.
+          I'm looking to work with people that will help me grow as a persona
+          and a programmer, as well as learn stuff about new cultures.
         </span>
       </div>
       {/* about me div */}
@@ -48,19 +51,31 @@ const About = () => {
           eligendi nulla.
         </span>
       </div>
-      {/* essentials div */}
-      <div>
+      <div className="about-stats">
         <div>
-          <span>essentials</span>
-        </div>
-        <span>all my stats here</span>
-      </div>
-      <div className="about-main">
-      <div>
           <span>statistics</span>
         </div>
         <span>all my stats here</span>
       </div>
+      <div className="about-stats">
+        <div>
+          <span>hobbies</span>
+        </div>
+        <span>all my stats here</span>
+      </div>
+      <div className="about-stats">
+        <div>
+          <span>likes</span>
+        </div>
+        <span>all my stats here</span>
+      </div>
+      <div className="about-stats">
+        <div>
+          <span>favorite music</span>
+        </div>
+        <span>all my stats here</span>
+      </div>
+      <div className="space"></div>
     </div>
   );
 };
